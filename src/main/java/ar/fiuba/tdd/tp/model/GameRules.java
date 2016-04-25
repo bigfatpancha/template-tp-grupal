dbe98a5ef7529a9428bbc76d11fb8792e9f752de
Maria Agustina Markosich <ma.markosich@yahoo.com.ar>
2016-04-25 20:55:54 -0300

    cargo reglas hardcodeadas

--- src/main/java/ar/fiuba/tdd/tp/model/GameRules.java
+++ src/main/java/ar/fiuba/tdd/tp/model/GameRules.java
@@ -1,16 +1,38 @@
 package ar.fiuba.tdd.tp.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by mariagustina on 23/04/16.
  */
 public class GameRules {
+
+    private Map<String,String> rules;
 
     public GameRules() {
 
     }
 
     public void load(String gameName) {
-        System.out.print(gameName);
+
+        //TODO: Levantarlo del json como diccionario de diccionarios!
+
+        //Map<String, Map<String,String>> map1 = new HashMap<String, String>();
+
+        Map<String,String> map2 = new HashMap<String, String>();
+
+        map2.put("event1", "look around");
+
+        map2.put("event2", "pick stick");
+
+        this.rules = map2;
+
+        //map1.put("UserEvents", map2);
+
+        //System.out.println(map.get("dog"));
+
+        System.out.print(this.rules);
     }
 }
 
