fdf4826864e7c7ff1665cf1f95497c4c7df5a98c
Lucía <luia.julia.r@gmail.com>
2016-04-24 21:20:47 -0300

    agrego la clase creadora del Fetch Request

---
+++ src/main/java/ar/fiuba/tdd/tp/model/FetchQuestCreator.java
@@ -0,0 +1,13 @@
+package ar.fiuba.tdd.tp.model;
+
+/**
+ * Created by Lucía on 23/4/2016.
+ */
+public class FetchQuestCreator extends GameCreator {
+
+    @Override
+    public Game crateGame(String gameName) {
+
+        return new FetchQuestGame(gameName);
+    }
+}
