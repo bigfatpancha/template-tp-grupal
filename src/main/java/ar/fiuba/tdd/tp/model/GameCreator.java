41955447f6a29112e00fcdc8ae5919e097658911
Lucía <luia.julia.r@gmail.com>
2016-04-24 21:22:59 -0300

    se agrega la clase gameCreator

---
+++ src/main/java/ar/fiuba/tdd/tp/model/GameCreator.java
@@ -0,0 +1,10 @@
+package ar.fiuba.tdd.tp.model;
+
+/**
+ * Created by mariagustina on 23/04/16.
+ */
+public abstract class GameCreator {
+
+    public abstract Game crateGame(String gameName);
+
+}
--- src/main/java/ar/fiuba/tdd/tp/model/GameFactory.java
+++
@@ -1,9 +0,0 @@
-package ar.fiuba.tdd.tp.model;
-
-/**
- * Created by mariagustina on 23/04/16.
- */
-public class GameFactory {
-
-
-}
