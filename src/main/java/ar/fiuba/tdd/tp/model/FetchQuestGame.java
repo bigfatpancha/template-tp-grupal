c786f8b5f2db976a0dc5671c8b5a6b0bfad51d94
Lucía <luia.julia.r@gmail.com>
2016-04-24 21:21:19 -0300

    agrego la clase FetchRequestGame

---
+++ src/main/java/ar/fiuba/tdd/tp/model/FetchQuestGame.java
@@ -0,0 +1,11 @@
+package ar.fiuba.tdd.tp.model;
+
+/**
+ * Created by Lucía on 24/4/2016.
+ */
+public class FetchQuestGame extends Game {
+
+    public FetchQuestGame(String gameName) {
+        super(gameName);
+    }
+}
