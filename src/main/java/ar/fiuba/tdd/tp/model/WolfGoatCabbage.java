ac2c7b48d3e76954b5384cc5ef95e3b265398632
Lucía <luia.julia.r@gmail.com>
2016-04-27 02:08:55 -0300

    creator y clase para el juego lobo cabra col

---
+++ src/main/java/ar/fiuba/tdd/tp/model/WolfGoatCabbage.java
@@ -0,0 +1,25 @@
+package ar.fiuba.tdd.tp.model;
+
+/**
+ * Created by Lucía on 27/4/2016.
+ */
+public class WolfGoatCabbage extends Game {
+    public WolfGoatCabbage() {
+        super();
+    }
+
+    @Override
+    public Game loadRules(String gameName) {
+        return super.loadRules(gameName);
+    }
+
+    @Override
+    public void continueGame() {
+        //TODO
+    }
+
+    @Override
+    public void consultGameRules() {
+        //TODO
+    }
+}
